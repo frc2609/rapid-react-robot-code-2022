95053da6adbaaea81c0f7e354997cb3a0c3fb6d6
ferntemp <email@email.com>
2022-02-12 13:39:12 -0500

    Added new codebase

---
+++ README.md
@@ -0,0 +1,41 @@
+# rapid-react-robot-code-2022
+
+Robot code for First Robotics Challenge for 2022.
+
+The `pictures` folder is for the embedded photos. Don't delete it.
+
+## Prerequisites
+
+Install the latest version of WPILib 2022 from 
+[this link](https://github.com/wpilibsuite/allwpilib/releases/tag/v2022.3.1)
+
+Then, install the NI Game Tools for First Robotics from
+[this link](https://docs.wpilib.org/en/stable/docs/zero-to-robot/step-2/frc-game-tools.html)
+
+## Deploying code to the robot
+
+First, turn on the robot using the circuit breaker:
+
+![](pictures/breaker.jpg)
+
+Wait until the router light turns yellow (it will take about a minute)
+
+![](pictures/router.jpg)
+
+Next, go to Wi-Fi settings and connect to "2609_widetestbot"
+
+![](pictures/wifi_setting.jpg)
+
+After that, press `CTRL+SHIFT+P` or press the WPILib button in the top right corner and type "Deploy Robot Code", then press enter.
+
+![](pictures/deploy.png)
+
+If you get an error saying `BUILD FAILED in Xs`, you are probably not connected to the robot. Scroll up in the console. 
+
+If you see something along the lines of `Are you connected to the robot, and is it on?` You are likely not connected to the robot's Wi-Fi network.
+
+![](pictures/no_connection.png)
+
+If you get a message saying `BUILD SUCCESSFUL in Xs`, your code is now deployed to the robot and ready to be used.
+
+![](pictures/success.png)
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,65 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+import frc.robot.commands.SetArm;
+import frc.robot.commands.ExampleCommand;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.ColorSensing;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // joysticks and buttons
+  public final Joystick driveJoystick = new Joystick(0);
+  public final JoystickButton climbButton = new JoystickButton(driveJoystick, 3); // X on xbox controller
+  //public final JoystickButton ejectBallButton = new JoystickButton(driveJoystick, );
+  // subsystems
+  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final Drive m_driveSubsystem = new Drive(driveJoystick);
+  private final Climber m_climbSubsystem = new Climber();
+  private final ColorSensing m_colorSubsystem = new ColorSensing();
+  // commands
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    climbButton.whenPressed(new SetArm(m_climbSubsystem, 53.15));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
