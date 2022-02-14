c6c7b23de7bb1b45e888850cdf306273f72883f4
Aydan Boynton <aj01cars@outlook.com>
2022-02-14 17:24:10 -0500

    added constants for motors and controller
    Co-authored-by: FernTemp <FernTemp@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,30 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    // XBox controller
+    public static final int LEFT_STICK_X_AXIS = 0;
+    public static final int LEFT_STICK_Y_AXIS = 1;
+    public static final int RIGHT_STICK_X_AXIS = 4;
+    public static final int RIGHT_STICK_Y_AXIS = 5;
+    public static final int LEFT_TRIGGER_AXIS = 2;
+    public static final int RIGHT_TRIGGER_AXIS = 3;
+    public static final int A_BUTTON = 1;
+    public static final int B_BUTTON = 2;
+    public static final int X_BUTTON = 3;
+    public static final int Y_BUTTON = 4;
+    public static final int START_BUTTON = 8;
+    public static final int BACK_BUTTON = 7;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int LEFT_STICK_BUTTON = 9;
+    public static final int RIGHT_STICK_BUTTON = 10;
+
+    // Motors
+    public static final int HOOK_MOTOR = 6;
+    public static final int BAR_MOTOR = 5;
+    public static final int LEFT_FRONT_MOTOR = 3;
+    public static final int LEFT_REAR_MOTOR = 4;
+    public static final int RIGHT_FRONT_MOTOR = 2;
+    public static final int RIGHT_REAR_MOTOR = 1;
+}
