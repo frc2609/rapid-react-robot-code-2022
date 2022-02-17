b0f676a5a68edd51550a0c17f2dbe37927bde5f5
Aydan Boynton <aj01cars@outlook.com>
2022-02-17 17:09:22 -0500

    added deadzone

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,6 +7,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.Constants;
 
+import java.lang.Math;
+
 public class Shooter extends SubsystemBase {
   private final CANSparkMax shooterLeftMotor = new CANSparkMax(Constants.SHOOTER_LEFT_MOTOR, MotorType.kBrushless);
   private final CANSparkMax shooterRightMotor = new CANSparkMax(Constants.SHOOTER_RIGHT_MOTOR, MotorType.kBrushless);
@@ -20,13 +22,14 @@ public class Shooter extends SubsystemBase {
   @Override
   public void periodic()
   {
-    setMotors(m_stick.getRawAxis(Constants.RIGHT_STICK_X_AXIS));
+    double move = m_stick.getRawAxis(Constants.RIGHT_STICK_X_AXIS);
+    setMotors(Math.abs(move) < Constants.JOYSTICK_DRIFT_TOLERANCE ? 0 : move);
   }
 
   public void setMotors(double set)
   {
-    shooterLeftMotor.set(set);
-    shooterRightMotor.set(-set);
+    shooterLeftMotor.set(-set);
+    shooterRightMotor.set(set);
   }
 
 }
