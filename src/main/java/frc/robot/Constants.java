dd4e11b9ae1e19930294bce9fff6edd390b2fdb1
Aydan Boynton <aj01cars@outlook.com>
2022-03-22 22:32:46 -0400

    lower prox threshold

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -142,7 +142,7 @@ public final class Constants {
         public static final double kRamseteB = 2;
         public static final double kRamseteZeta = 0.7;
 
-        public static final int proxThreshold = 120;
+        public static final int proxThreshold = 100;
     
         public static boolean isReversed = false;
         public static double rpmTolerance = 200;
