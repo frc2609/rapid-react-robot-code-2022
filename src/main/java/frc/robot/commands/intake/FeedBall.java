9e3cf35d19ccf7a6ea2ff9f3b0e4145b5ddedc36
msata <msata@msata.com>
2022-03-23 14:46:41 -0400

    stop belts when command ends

--- src/main/java/frc/robot/commands/intake/FeedBall.java
+++ src/main/java/frc/robot/commands/intake/FeedBall.java
@@ -31,7 +31,7 @@ public class FeedBall extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    // m_intake.setBelts(0.0);
+    m_intake.setBelts(0.0);
   }
 
   // Returns true when the command should end.
