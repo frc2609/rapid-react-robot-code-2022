ba7ab40124778e20097a7de9ceb84b51b327416a
Aydan Boynton <aj01cars@outlook.com>
2022-03-24 12:25:57 -0400

    changed pid values, lock drive is tighter

--- src/main/java/frc/robot/commands/LockDrive.java
+++ src/main/java/frc/robot/commands/LockDrive.java
@@ -19,9 +19,9 @@ public class LockDrive extends CommandBase {
 
   public LockDrive() {
     // Use addRequirements() here to declare subsystem dependencies.
-    rightDrivePID = new SimPID(0.02, 0, 0.3);
-    leftDrivePID = new SimPID(0.02, 0, 0.3);
-    headingPID = new SimPID(0.02, 0, 0.3);
+    rightDrivePID = new SimPID(0.016, 0, 0.3);
+    leftDrivePID = new SimPID(0.016, 0, 0.3);
+    headingPID = new SimPID(0.016, 0, 0.3);
   }
 
   // Called when the command is initially scheduled.
