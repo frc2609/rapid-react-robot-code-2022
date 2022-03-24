efc7ec9cee830cfffa1e4656a8107d43e2a9938d
Aydan Boynton <aj01cars@outlook.com>
2022-03-24 11:24:03 -0400

    init underglow subsystem

---
+++ src/main/java/frc/robot/subsystems/Underglow.java
@@ -0,0 +1,19 @@
+// package frc.robot.subsystems;
+
+// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+// import frc.robot.Constants;
+
+// public class Underglow extends SubsystemBase {
+//     // led controller pretends to be a PWM motor controller
+//     private final PWMSparkMax ledController = new PWMSparkMax(Constants.PwmMotorId.LED_CONTROLLER);
+
+//     public Underglow() {
+
+//     }
+
+//     @Override
+//     public void periodic() {
+//         bool team =
+//     }
+// }
