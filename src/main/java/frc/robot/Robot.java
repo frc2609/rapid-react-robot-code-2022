a5b1c8bfe2b58337d77cee74a77494b6fa54e7d1
msata <msata@msata.com>
2022-03-28 19:38:36 -0400

    remove usb webcam

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -43,7 +43,6 @@ public class Robot extends TimedRobot {
 
     SmartDashboard.putBoolean(Constants.INTAKE_OVERRIDE_STRING, false);
     SmartDashboard.putBoolean(Constants.FEEDER_OVERRIDE_STRING, false);
-    CameraServer.startAutomaticCapture();
     RobotContainer.m_shooterSubsystem.disableAutoAim();
   }
 
